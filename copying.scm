c4bae738769341f96b793c735b4155ecd9c5b3de
K K Lin <klin@math.arizona.edu>
2016-07-24 13:05:54 -0700

    copying.scm   : format + fix year

--- copying.scm
+++ copying.scm
@@ -1,25 +1,26 @@
 #|
 
-This file is part of DIFFEOM, a system for solving differential equations on
-manifolds.
+This file is part of DIFFEOM, a system for solving
+differential equations on manifolds.
 
-Copyright (C) 2009 by Kevin K Lin <klin@math.arizona.edu>
+Copyright (C) 2016 by Kevin K Lin
+<kkylin@alum.mit.edu>
 
-This program is free software; you can redistribute it
-and/or modify it under the terms of the GNU General Public
-License as published by the Free Software Foundation; either
-version 2 of the License, or (at your option) any later
-version.
+This program is free software; you can redistribute
+it and/or modify it under the terms of the GNU
+General Public License as published by the Free
+Software Foundation; either version 2 of the
+License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be
-useful, but WITHOUT ANY WARRANTY; without even the implied
-warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-PURPOSE.  See the GNU General Public License for more
-details.
+This program is distributed in the hope that it
+will be useful, but WITHOUT ANY WARRANTY; without
+even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
 
-You should have received a copy of the GNU General Public
-License along with this program; if not, write to the Free
-Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-Boston, MA 02110-1301 USA.
+You should have received a copy of the GNU General
+Public License along with this program; if not, write
+to the Free Software Foundation, Inc., 51 Franklin
+Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 |#
